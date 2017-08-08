7280ff12455b13367fb77cac82d8d42f4c37ea70
venkatesh.poosarla <venkatesh.poosarla@one.verizon.com>
2017-08-07 19:38:12 -0700

    working code of solr hdfs

--- dal/src/main/scala/com/verizon/bda/trapezium/dal/solr/SolrOpsHdfs.scala
+++ dal/src/main/scala/com/verizon/bda/trapezium/dal/solr/SolrOpsHdfs.scala
@@ -1,10 +1,14 @@
-package com.verizon.bda.trapezium.dal.solr
+package com1
 
 import com.typesafe.config.Config
+import com.verizon.bda.trapezium.dal.solr.SolrOps
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.log4j.Logger
 
+import scala.collection.mutable.ListBuffer
 import scala.xml.XML
-import scalaj.http.{Http, HttpResponse}
+import scalaj.http.Http
 
 /**
   * Created by venkatesh on 8/3/17.
@@ -14,17 +18,17 @@ class SolrOpsHdfs(config: Config) extends SolrOps(config: Config) {
   var map: Map[String, String] = null
 
   override def createCollection(): Unit = {
-    val solrConfigName = configName
-
     val host = config.getStringList("solrNodeHosts").get(0)
     val solrServerUrl = "http://" + host + ":8983/solr/admin/collections"
-    val url = Http(solrServerUrl).timeout(connTimeoutMs = 2000, readTimeoutMs = 5000)
+    val url = Http(solrServerUrl).timeout(connTimeoutMs = 20000, readTimeoutMs = 50000)
     log.info(s"deleting collection${collectionName} if exists")
-    val response1: HttpResponse[String] = url.param("name", collectionName).param("action", "DELETE").asString
+    val response1: scalaj.http.HttpResponse[String] = url
+                                                        .param("name", collectionName)
+                                                        .param("action", "DELETE").asString
     log.info(s"deleting collection response ${response1.body}")
     log.info(s"creating collection : ${collectionName} ")
 
-    val response: HttpResponse[String] = url.param("action", "CREATE")
+    val response: scalaj.http.HttpResponse[String] = url.param("action", "CREATE")
       .param("name", collectionName)
       .param("numShards", "10")
       .param("replicationFactor", "1")
@@ -32,7 +36,9 @@ class SolrOpsHdfs(config: Config) extends SolrOps(config: Config) {
       .param("collection.configName", configName).asString
     log.info(s"creating collection response ${response.body}")
     val xmlBody = XML.loadString(response.body)
-    val ips = (xmlBody \\ "lst").map(p => p \ "@name").map(_.text.split("_")(0)).filter(p => p != "responseHeader" && p != "success")
+    val ips = (xmlBody \\ "lst").map(p => p \ "@name")
+                                .map(_.text.split("_")(0))
+                                .filter(p => p != "responseHeader" && p != "success")
 
     val coreNames = (xmlBody \\ "str").map(p => p.text)
     map = (coreNames, ips).zipped.toMap
@@ -44,35 +50,39 @@ class SolrOpsHdfs(config: Config) extends SolrOps(config: Config) {
   }
 
   override def createCores(): Unit = {
-    var reponse: HttpResponse[String] = null
+    //    var reponse: HttpResponse[String] = null
     val nameNode = config.getString("nameNode")
     val hdfsHome = s"hdfs://${nameNode}"
     val indexedPath = config.getString("indexFilesPath")
     log.info("creating cores")
+    val list = new ListBuffer[String]
     for ((coreName, host) <- map) {
+      val client = HttpClientBuilder.create().build()
+
       unloadCore(host, coreName)
-      log.info(s"creating core:${coreName}")
-      var retry = 0
-      //do {
-        val solrServerUrl = (s"http://${host}/solr/admin/cores")
-        val url = Http(solrServerUrl).timeout(connTimeoutMs = 2000, readTimeoutMs = 5000)
-        val tmp = coreName.split("_")
-        val shard_index = tmp(tmp.size - 2).substring(5).toInt
-        val directory = s"$hdfsHome$indexedPath/part-${shard_index - 1}"
-
-        reponse = url.param("action", "CREATE")
-          .param("name", coreName)
-          .param("dataDir", directory)
-          .param("collection", collectionName)
-          .param("collection.configName", configName)
-          //          .param("shard", s"shard$shard_index")
-          //          .param("numshard", s"$shard_index" )
-          .asString
-        Thread.sleep(1000)
-        log.info(s"creating core response ${reponse.body} with coreName:${coreName} and directory${directory}")
-        retry = retry + 1
-     // } while (!reponse.is2xx && retry < 3)
+      val tmp = coreName.split("_")
+      val shard_index = tmp(tmp.size - 2).substring(5).toInt
+      val directory = s"$hdfsHome$indexedPath/part-${shard_index - 1}"
+      val url = s"http://${host}/solr/admin/cores?" +
+        "action=CREATE&" +
+        s"collection=${collectionName}&" +
+        s"collection.configName=${configName}&" +
+        s"name=${coreName}&" +
+        s"dataDir=${directory}"
+      list.append(url)
     }
+    makCoreCreation(list.toList)
+  }
 
+  def makCoreCreation(list: List[String]): Unit = {
+    list.foreach(url => {
+      val client = HttpClientBuilder.create().build()
+      println(url)
+      val request = new HttpGet(url)
+      val response = client.execute(request)
+      log.info(s"response: ${response} ")
+      client.close()
+      response.close()
+    })
   }
 }
