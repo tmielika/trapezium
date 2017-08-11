a95387ffbfc3fee7e2e76ee15b7c1ac612146ba8
venkatesh.poosarla <venkatesh.poosarla@one.verizon.com>
2017-08-11 15:53:33 -0700

    removed shell

--- dal/src/main/scripts/solr.sh
+++
@@ -1,3 +0,0 @@
-export JAVA_HOME=/opt/bda/java/jdk1.8.0_102/
-export PATH=$JAVA_HOME/bin:$PATH
-java -cp solrtest-1.0-SNAPSHOT.jar:solrtest-1.0-SNAPSHOT-job.jar:/etc/hadoop/conf/   com.verizon.bda.trapezium.dal.solr.SolrTests
